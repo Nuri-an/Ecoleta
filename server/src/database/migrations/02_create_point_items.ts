871be31f6f1830ee9054b7bf0a8c80c643ab3393
Nurian <nuriancoelho@hotmail.com>
2020-06-03 14:56:16 -0300

    back-end da aplicaação

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+
+export async function up(Knex: Knex) {
+    //crir tabela
+   return Knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();
+        table.integer('point_id').notNullable().references('id').inTable('points');
+        table.integer('item_id').notNullable().references('id').inTable('items');
+    });
+}
+
+export async function down(Knex: Knex) {
+    //voltar atras (deletar tabela)
+    return Knex.schema.dropTable('point_items');
+}
